dbd2a72ff1aa636c0b9e937b3c8aefa5e80f1481
Murray Champernowne <murrayadcock@gmail.com>
2024-06-12 16:49:29 +0100

    feat: initial commit

--- app/root.tsx
+++ app/root.tsx
@@ -1,29 +1,148 @@
+/* eslint-disable jsx-a11y/no-redundant-roles */
+import groq from "groq";
+import { loadQuery } from "~/sanity/loader.server";
 import {
-  Links,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
+	Links,
+	Meta,
+	Outlet,
+	Scripts,
+	ScrollRestoration,
+	useLoaderData,
 } from "@remix-run/react";
+import { cssBundleHref } from "@remix-run/css-bundle";
+import type { LinksFunction } from "@remix-run/node";
+import type { SanityDocument } from "@sanity/client";
+
+import { Logo } from "~/svg/Logo";
+import SearchIcon from "~/svg/MagnifyingGlass";
+import Image from "~/components/image/Image";
+
+import reset from "~/styles/reset.css?url";
+import theme from "~/styles/theme.css?url";
+
+export const loader = async () => {
+	const { data } = await loadQuery<SanityDocument[]>(
+		groq`*[_type == "global"]`
+	);
+
+	return { data };
+};
 
 export function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        {children}
-        <ScrollRestoration />
-        <Scripts />
-      </body>
-    </html>
-  );
+	const { data } = useLoaderData<typeof loader>();
+	const global = data[0];
+
+	return (
+		<html lang="en">
+			<head>
+				<meta charSet="utf-8" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<Meta />
+				<Links />
+				<link
+					href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap"
+					rel="stylesheet"
+				/>
+				<link
+					href="https://fonts.googleapis.com/css2?family=Space+Mono:wght@400;700&display=swap"
+					rel="stylesheet"
+				/>
+			</head>
+			<body>
+				<header className="global-header">
+					<Logo />
+					<a href="/search" aria-label="Search">
+						<SearchIcon />
+					</a>
+					<button id="globalMenuButton" type="button">
+						Menu
+					</button>
+
+					<dialog id="globalMenuModal">
+						<header>
+							<Logo fill="var(--c-text-invert)" />
+							<form method="dialog">
+								<button type="submit">Close</button>
+							</form>
+						</header>
+						<p>For testing purposes only.</p>
+						<nav>
+							<ul role="list">
+								<li>Home</li>
+								<li>About</li>
+								<li>Contact</li>
+							</ul>
+						</nav>
+					</dialog>
+				</header>
+				{children}
+				<footer className="global-footer">
+					<section>
+						<Logo />
+						<p>{global.footer_description}</p>
+						<ul role="list">
+							{global.footer_socials?.map((social: any, index: number) => (
+								<li key={`footer-link-${index}`}>
+									<a href={social.url}>
+										<Image image={social.logo?.[0]} />
+									</a>
+								</li>
+							))}
+						</ul>
+					</section>
+					<form method="POST">
+						<label htmlFor="newsletter">{global.footer_newsletter}</label>
+						<input
+							id="newsletter"
+							type="email"
+							name="email"
+							placeholder="Email address"
+							required
+						/>
+						<button type="submit">Subscribe</button>
+					</form>
+					<nav aria-label="Footer menu.">
+						{global.footer_menus?.map((menu: any, index: number) => {
+							return (
+								<div key={`footer-menu-${index}`}>
+									<h2 className="u-eyebrow">{menu.title}</h2>
+									<ul role="list">
+										{menu.links.map((link: any, index: number) => (
+											<li key={`footer-menu-link-${index}-${link.label}`}>
+												<a href={link.url}>{link.label}</a>
+											</li>
+										))}
+									</ul>
+								</div>
+							);
+						})}
+					</nav>
+					<section>
+						<p>{global.footer_copyright}</p>
+						<ul role="list">
+							{global.footer_terms?.[0]?.links?.map(
+								(link: any, index: number) => (
+									<li key={`footer-menu-terms-${index}`}>
+										<a href={link.url}>{link.label}</a>
+									</li>
+								)
+							)}
+						</ul>
+					</section>
+				</footer>
+				<ScrollRestoration />
+				<Scripts />
+			</body>
+		</html>
+	);
 }
 
 export default function App() {
-  return <Outlet />;
+	return <Outlet />;
 }
+
+export const links: LinksFunction = () => [
+	...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+	{ rel: "stylesheet", href: reset },
+	{ rel: "stylesheet", href: theme },
+];
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,80 @@
+import groq from "groq";
+import { useLoaderData } from "@remix-run/react";
+import { loadQuery } from "~/sanity/loader.server";
+
 import type { MetaFunction } from "@remix-run/node";
+import type { SanityDocument } from "@sanity/client";
+
+import HeroArrow from "~/svg/RightDownArrow";
+import CTA from "~/components/cta/CTA";
+import PageCard from "~/components/card-page/CardPage";
+import Banner from "~/components/banner/Banner";
+import GridLogo from "~/components/grid-logo/GridLogo";
+import GridRevelation from "~/components/grid-revelation/GridRevelation";
 
 export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
+	return [
+		{ title: "Appnovation" },
+		{ name: "description", content: "Welcome to Appnovation!" },
+	];
+};
+
+export const loader = async () => {
+	const { data } = await loadQuery<SanityDocument[]>(
+		groq`*[_type == "homepage"]`
+	);
+
+	return { data };
 };
 
 export default function Index() {
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/start/quickstart"
-            rel="noreferrer"
-          >
-            5m Quick Start
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/start/tutorial"
-            rel="noreferrer"
-          >
-            30m Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+	const { data } = useLoaderData<typeof loader>();
+	const homepage = data[0];
+
+	return (
+		<main>
+			<section className="hero">
+				<HeroArrow />
+				<h1 className="u-eyebrow">
+					{homepage.title}
+					<br />
+					{homepage.subtitle}
+				</h1>
+				<p>{homepage.copy}</p>
+				<div>
+					{homepage.links.map((link: any, index: number) => (
+						<CTA
+							key={link.label}
+							url={link.url}
+							label={link.label}
+							type={index === 1 ? "highlight" : undefined}
+						/>
+					))}
+				</div>
+			</section>
+			<section className="highlight-grid">
+				{homepage.highlights.map((highlight: any) => (
+					<PageCard
+						{...highlight}
+						desc={highlight.description}
+						url={`/${highlight.slug.current}`}
+						thumb={highlight.thumbnail?.[0]}
+						key={highlight.description}
+					/>
+				))}
+			</section>
+			{homepage.content?.map((block: any) => {
+				switch (block._type) {
+					case "revelation_grid":
+						return <GridRevelation {...block} />;
+					case "logo_grid":
+						return <GridLogo {...block} />;
+					case "banner":
+						return <Banner {...block} />;
+					default:
+						return <p>Unknown Component</p>;
+				}
+			})}
+		</main>
+	);
 }
