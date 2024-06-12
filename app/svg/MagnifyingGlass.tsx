3ff518a52c594acb038908e1c93ae18d7f5b2552
Murray Champernowne <murrayadcock@gmail.com>
2024-06-12 16:50:42 +0100

    feat: svg assests

---
+++ app/svg/MagnifyingGlass.tsx
@@ -0,0 +1,20 @@
+export const MagnifyingGlass = () => {
+	return (
+		<svg
+			width="24"
+			height="24"
+			viewBox="0 0 24 24"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg">
+			<circle
+				cx="9.5"
+				cy="9.5"
+				r="6.5"
+				stroke="currentColor"
+				strokeWidth="2"></circle>
+			<path d="M14 14L21 21" stroke="currentColor" strokeWidth="2"></path>
+		</svg>
+	);
+};
+
+export default MagnifyingGlass;
