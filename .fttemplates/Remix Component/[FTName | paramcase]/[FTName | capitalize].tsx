7b5964872de962117bd34042b12f0bd10e072b8d
Murray Champernowne <murrayadcock@gmail.com>
2024-06-12 16:49:45 +0100

    feat: file templates

---
+++ .fttemplates/Remix Component/[FTName | paramcase]/[FTName | capitalize].tsx
@@ -0,0 +1,15 @@
+import styles from "./[FTName | capitalize].module.css";
+
+interface Props {
+  title: string;
+}
+
+export const [FTName | capitalize] = ({ title }: Props) => {
+  
+  return (
+    <>
+    </>
+  )
+}
+
+export default [FTName | capitalize]
